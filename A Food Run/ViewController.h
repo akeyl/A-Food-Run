68afb75e78e4641c7064b6068f41a1118fe94526
AK <ak@dell.local>
2016-01-16 15:08:06 -0800

    Initial Commit

---
+++ A Food Run/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  A Food Run
+//
+//  Created by AK on 1/16/16.
+//  Copyright © 2016 AK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ A Food Run/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  A Food Run
+//
+//  Created by AK on 1/16/16.
+//  Copyright © 2016 AK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
